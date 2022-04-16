e3a840c07b38bdab48cd1b4ab20d0a5d4e7a0834
Kenny Larsen <98322694+Ken9Kodehode@users.noreply.github.com>
2022-04-16 12:55:18 +0200

    ad

--- js/index.js
+++ js/index.js
@@ -18,7 +18,23 @@ function removeValue()
     document.getElementById('number').value = value;
 }
 
+//Change color randomly when hitting "pluss"
+
 var buton=document.getElementById("butonPluss");
+var allchar="0123456789ABCDEF";
+
+buton.addEventListener("click",changeColor);
+
+function changeColor(){
+  var  randcol= "";
+for(var i=0; i<6; i++){
+   randcol += allchar[Math.floor(Math.random()*16)];
+}
+document.body.style.backgroundColor= "#"+randcol;
+}
+
+//Change color randomly when hitting "minus"
+var buton=document.getElementById("butonMinus");
 var allchar="0123456789ABCDEF";
 
 buton.addEventListener("click",changeColor);
