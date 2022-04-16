ad38bb9274ab4761e99b5baee8305957f26b939a
Kenny Larsen <98322694+Ken9Kodehode@users.noreply.github.com>
2022-04-16 12:36:42 +0200

    Update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 //JavaScript skal skrives her :)
+
+let colorArray = ["AliceBlue", "Coral", "Crimson", "Yellow", "Lavender", "Chartreuse", "Fuchsia", "PaleGreen"]
+
+function addValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+
+function removeValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+}
+
+
+
