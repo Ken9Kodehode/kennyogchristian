5953dfb4c169d9cc6fda02b3e178d443d68caad9
Kenny Larsen <98322694+Ken9Kodehode@users.noreply.github.com>
2022-04-18 11:31:14 +0200

    update

--- js/index.js
+++ js/index.js
@@ -46,5 +46,7 @@ for(var i=0; i<6; i++){
    randcol += allchar[Math.floor(Math.random()*16)];
 }
 document.body.style.backgroundColor= "#"+randcol;
+                                                  //This can be used to grab the rectangle but need to put z-index: +1; in css first!
+                                                  //document.getElementById("test").style.backgroundColor= "#"+randcol;
 }
 
